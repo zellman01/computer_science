9d7489b428cf2a63d9db994a877952a44d101400
Zach <18561222+zellman01@users.noreply.github.com>
2021-02-24 12:56:00 -0500

    Update Airport.cpp

--- C++ Projects/Projects/Comp 2270/Airplane Simulator (P2)/Airport.cpp
+++ C++ Projects/Projects/Comp 2270/Airplane Simulator (P2)/Airport.cpp
@@ -26,7 +26,7 @@ void Airport::update() {
 }
 
 void Airport::landingPlane(Airplane & obj) {
-	landing->insertNode(obj, airplaneNumber);
+	landing->insertNode(obj, airplaneNumber); // Turn into a boolean, and if false, update the planes refused
 	airplaneNumber++;
 	landing->sort();
 }
@@ -47,7 +47,17 @@ void Airport::departedPlane() {
 }
 
 void Airport::view(int timeUnits) {
+	int spaces = 50;
 	cout << "Simulation ended after " << timeUnits << " time units." << endl;
-	cout << "Total number of planes processed:" << setw(50) << airplaneNumber-1 << endl;
-	
+	cout << "Total number of planes processed:" << setw(spaces) << airplaneNumber-1 << endl;
+	cout << "Number of planes landed:" << setw(spaces) << planesLanded << endl;
+	cout << "Number of planes taken off:" << setw(spaces) << planesTakenOff << endl;
+	cout << "Number of planes refused to land:" << setw(spaces) << planesRefused << endl;
+	cout << "Number left ready to land:" << setw(spaces) << landing->getSize() << endl;
+	cout << "Number left ready to depart:" << setw(spaces) << takeOff->getSize() << endl;
+	cout << "Number crashed:" << setw(spaces) << planesCrashed << endl;
+	cout << "Percentage of time runway was idle:" << setw(spaces) << endl; // Create a new var, and then proceed to add one every time the runway is doing 
+	//nothing, then divide by the timeUnits
+	cout << "Average wait time to land:" << setw(spaces) << endl; // Figure out
+	cout << "Average wait time to takeoff:" << setw(spaces) << endl; // Figure out
 }
