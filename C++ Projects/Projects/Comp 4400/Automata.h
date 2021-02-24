e6b39550a8938e047b65edcb7980ba75227ef90b
Zach <18561222+zellman01@users.noreply.github.com>
2021-02-24 13:09:58 -0500

    Update Automata.h

--- C++ Projects/Projects/Comp 4400/Automata.h
+++ C++ Projects/Projects/Comp 4400/Automata.h
@@ -1,10 +1,30 @@
 #ifndef AUTOMATA_H
 #define AUTOMATA_H
 
+#include <vector>
+#include <string>
+#include <map>
+
 class Automata
 {
 	public:
-	protected:
+		Automata();
+		void addState();
+		void addSymbol();
+		void addTransition();
+		void setInitialState();
+		void addFinalState();
+		void description();
+		void validSymbol();
+		void currectState(); // Return value must be the current state
+		void changeStates();
+		void isFinal(); // Return value must indicate if it is a final state or not
+	private:
+		std::vector<std::string> states; // https://www.geeksforgeeks.org/vector-in-cpp-stl/
+		std::vector<std::string> inputSymbols;
+		std::vector<std::string> finalStates;
+		std::map<std::string, std::string> transitions; // https://www.geeksforgeeks.org/map-associative-containers-the-c-standard-template-library-stl/
+		std::string currentState;
 };
 
 #endif
