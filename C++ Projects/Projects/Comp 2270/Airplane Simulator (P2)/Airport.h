5174bc64e66833ca39262d2be563a374cf0965ef
Zach <18561222+zellman01@users.noreply.github.com>
2021-02-24 12:26:58 -0500

    Update Airport.h

--- C++ Projects/Projects/Comp 2270/Airplane Simulator (P2)/Airport.h
+++ C++ Projects/Projects/Comp 2270/Airplane Simulator (P2)/Airport.h
@@ -12,8 +12,7 @@ class Airport
 		void landedPlane(); // Will always assume that it is the top of the landing queue
 		void departingPlane(Airplane&);
 	private:
-		Queue * landing; // Planes requesting to land
-		Queue * departing; // Planes requesting to take off
+		Queue * landing, takeOff; // Planes requesting to land, and to take off
 		int airplaneNumber;
 };
 
