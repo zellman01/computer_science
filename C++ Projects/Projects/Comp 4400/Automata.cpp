ec42945222402ea303c56a483529e2f8a47b8368
Zach <18561222+zellman01@users.noreply.github.com>
2021-02-24 13:16:20 -0500

    Update Automata.cpp

--- C++ Projects/Projects/Comp 4400/Automata.cpp
+++ C++ Projects/Projects/Comp 4400/Automata.cpp
@@ -1,1 +1,47 @@
 #include "Automata.h"
+
+using namespace std;
+
+Automata::Automata() {
+  
+}
+
+void Automata::addState() {
+  
+}
+
+void Automata::addSymbol() {
+  
+}
+
+void Automata::addTransition() {
+  
+}
+
+void Automata::setInitialState(string initState) {
+  currentState = initState;
+}
+
+void Automata::addFinalState() {
+  
+}
+
+void Automata::description() {
+  
+}
+
+void Automata::validSymbol() {
+  
+}
+
+string Automata::currentState() {
+  return currentState;
+}
+
+void Automata::changeStates() {
+  
+}
+
+bool Automata::isFinal() {
+  
+}
