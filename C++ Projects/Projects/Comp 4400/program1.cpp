18d46eb3740864e49ae09a885c1ebf2b15e3974e
Zach <18561222+zellman01@users.noreply.github.com>
2021-03-03 13:49:45 -0500

    Update program1.cpp

--- C++ Projects/Projects/Comp 4400/program1.cpp
+++ C++ Projects/Projects/Comp 4400/program1.cpp
@@ -2,9 +2,16 @@
 #include <fstream>
 #include "Automata.h"
 
+using namespace std;
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	std::cout << "test";
+	if (argc != 2) {
+		cout << "usage: <prog_name> <file_name>" << endl;
+		return -1;
+	}
+	const string state = "<states>", alphabet = "<alphabet>", transition = "<transitions>", initState = "<initial state>", finalStates "<final states>";
+	fstream file(argv[1]); // Should pull the file out correctly
 	return 0;
 }
