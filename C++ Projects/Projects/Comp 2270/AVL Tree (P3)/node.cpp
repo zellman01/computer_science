5a20085ecbc50f92666e82e20b50408144a85287
Zach <18561222+zellman01@users.noreply.github.com>
2021-03-24 12:35:34 -0400

    Update node.cpp

--- C++ Projects/Projects/Comp 2270/AVL Tree (P3)/node.cpp
+++ C++ Projects/Projects/Comp 2270/AVL Tree (P3)/node.cpp
@@ -1,7 +1,8 @@
 #include "Node.h"
 
 Node::Node(int key) {
-	this.key = key;
+	this->key = key;
+	bf = 0; // If this ever becomes 2 or -2, balance must happen
 	left = nullptr;
 	right = nullptr;
 }
@@ -9,9 +10,25 @@ Node::Node(int key) {
 int Node::getKeyValue() { return key; }
 
 void Node::updateLeft(Node & leftNode) {
-	left = leftNode;
+	left = &leftNode;
 }
 
 void Node::updateRight(Node & rightNode) {
-	right = rightNode;
+	right = &rightNode;
+}
+
+Node * Node::getLeftPointer() {
+	return left;
+}
+
+Node * Node::getRightPointer() {
+	return right;
+}
+
+bool Node::checkForChildren() {
+	return (left == nullptr) && (right == nullptr);
+}
+
+void Node::updateKey(int key) {
+	this->key = key;
 }
