fe4f6dda95f46a12d66149f0a95c09e748686b9a
Zach <18561222+zellman01@users.noreply.github.com>
2021-03-30 13:45:01 -0400

    Add new functions

--- C++ Projects/Projects/Comp 2270/AVL Tree (P3)/AVLTree.h
+++ C++ Projects/Projects/Comp 2270/AVL Tree (P3)/AVLTree.h
@@ -9,17 +9,19 @@ class AVLTree
 		AVLTree();
 		void rotateLeft(Node&);
 		void rotateRight(Node &);
-		void rightBalance(Node&, bool &,Node&);
-		void leftBalance(Node&, bool &,Node&);
-		void insertAVL(Node*, Node*, bool&, Node&); // Needs to be worked on
-		void nodeDelete(int, Node*);
+		void rightBalance(Node&, bool &,Node&); // Needs to be worked on
+		void leftBalance(Node&, bool &,Node&); // Needs to be worked on
+		// Set both balances (and rotations) to return a Node* and then have it set the left or right pointer accordingly
+		void insertAVL(Node*, Node*, bool&, Node&);
+		Node *  nodeDelete(int, Node*); // Needs to be worked on
 		Node * searchNode(int, Node&);
 		void clearTree();
 		Node * getHeadNode();
 		void display(Node&,int=0);
 	private:
 		Node * headNode;
-		void _clearTree(Node*); // Work on later
+		Node * immedeatePred(Node*);
+		bool _clearTree(Node*);
 };
 
 #endif
