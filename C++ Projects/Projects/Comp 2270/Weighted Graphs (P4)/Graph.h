d539c510d9e9f7b29dff0053d761f5d5ba48b0e3
Zach <18561222+zellman01@users.noreply.github.com>
2021-04-12 15:06:05 -0400

    Update Graph.h

--- C++ Projects/Projects/Comp 2270/Weighted Graphs (P4)/Graph.h
+++ C++ Projects/Projects/Comp 2270/Weighted Graphs (P4)/Graph.h
@@ -2,6 +2,7 @@
 #define GRAPH_H
 
 #include <string>
+#include <vector>
 
 /*struct edge {
 	std::string pointa, pointb; // pointa is one node in the connection. pointb is the second node in the connection
@@ -21,12 +22,16 @@ class Graph
 		int searchNode(std::string);
 		void addNode(node);
 		void addEdge(std::string, std::string, int);
-		void mst();
+		void mst(); // Check for disjointed sets
+		bool disjoint(node, node);
 	private:
+		bool path(node, node, node, int);
+		bool cycle(node, node, node, bool);
 		node defaultNode;
 		node * nodeArray;
 		int ** edgeArray;
 		int graphSize, currentNodes, currentEdges;
+		std::vector<std::pair<std::pair<node, node>,int>> minSpanTree;
 };
 
 #endif
