714c8e229349671a248428e8bbc5c5fc15ded509
Zach <18561222+zellman01@users.noreply.github.com>
2021-04-12 15:05:34 -0400

    Update main.cpp

--- C++ Projects/Projects/Comp 2270/Weighted Graphs (P4)/main.cpp
+++ C++ Projects/Projects/Comp 2270/Weighted Graphs (P4)/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include "Graph.h"
 
 using namespace std;
@@ -8,14 +9,46 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	/*int graphSize = 0;
+	/*int graphSize = 0, nodeLine = 0;
 	cout << "Please insert the name of the file to use for the graph: ";
 	string input;
 	cin >> input;
 	fstream file(input);
+	bool weights = false, graphMade = false;
+	vector<node> nodes;
+	Graph * g;
 	while (getline(file, input)) {
-		if (file.peek() )
-	}*/
+		if (!weights) {
+			cin.ignore();
+			int var = -1;
+			var = file.peek();
+			if (!cin.fail()) {
+				weights = true;
+			} else {
+				node a;
+				a.name = input;
+				nodes.insert(nodes.end(), a);
+				graphSize++;
+			}
+		}
+		
+		if (weights && !graphMade) {
+			g = new Graph(graphSize);
+			for (int i = 0; i < nodes.size(); i++) {
+				g->addNode(nodes.at(i));
+			}
+			graphMade = true;
+		}
+		
+		if (weights) {
+			node a = nodes.at(nodeLine);
+			int weight = 0;
+			/*for (input >> weight) {
+				
+			}
+		}
+	}
+	g->print();*/
 	node a, b, c, d, e, f;
 	a.name = "A";
 	b.name = "B";
@@ -39,6 +72,7 @@ int main(int argc, char** argv) {
 	g.addEdge("D", "E", 7);
 	g.addEdge("D", "F", 8);
 	g.addEdge("E", "F", 2);
+	//g.addEdge("C", "E", 1);
 	g.print();
 	g.mst();
 	cout << endl;
