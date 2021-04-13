fc3710e8246e45c549e219fd97936641bdad5c01
Zach <18561222+zellman01@users.noreply.github.com>
2021-04-13 13:33:00 -0400

    Update main.cpp

--- C++ Projects/Projects/Comp 2270/Weighted Graphs (P4)/main.cpp
+++ C++ Projects/Projects/Comp 2270/Weighted Graphs (P4)/main.cpp
@@ -9,73 +9,37 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	/*int graphSize = 0, nodeLine = 0;
+	vector<string> nodeNames;
+	vector<node> nodeList;
 	cout << "Please insert the name of the file to use for the graph: ";
-	string input;
-	cin >> input;
-	fstream file(input);
-	bool weights = false, graphMade = false;
-	vector<node> nodes;
-	Graph * g;
-	while (getline(file, input)) {
-		if (!weights) {
-			cin.ignore();
-			int var = -1;
-			var = file.peek();
-			if (!cin.fail()) {
-				weights = true;
-			} else {
-				node a;
-				a.name = input;
-				nodes.insert(nodes.end(), a);
-				graphSize++;
-			}
-		}
-		
-		if (weights && !graphMade) {
-			g = new Graph(graphSize);
-			for (int i = 0; i < nodes.size(); i++) {
-				g->addNode(nodes.at(i));
-			}
-			graphMade = true;
-		}
-		
-		if (weights) {
-			node a = nodes.at(nodeLine);
-			int weight = 0;
-			/*for (input >> weight) {
-				
-			}
-		}
+	string inputFile;
+	cin >> inputFile;
+	fstream file(inputFile);
+	if (!file) {
+		cout << "File not found!";
+		return -1;
 	}
-	g->print();*/
-	node a, b, c, d, e, f;
-	a.name = "A";
-	b.name = "B";
-	c.name = "C";
-	d.name = "D";
-	e.name = "E";
-	f.name = "F";
-	Graph g(6);
-	g.addNode(a);
-	g.addNode(b);
-	g.addNode(c);
-	g.addNode(d);
-	g.addNode(e);
-	g.addNode(f);
-	g.addEdge("B","A", 5);
-	g.addEdge("A","C", 10);
-	g.addEdge("A", "D", 9);
-	g.addEdge("B", "D", 4);
-	g.addEdge("C", "D", 13);
-	g.addEdge("C", "E", 14);
-	g.addEdge("D", "E", 7);
-	g.addEdge("D", "F", 8);
-	g.addEdge("E", "F", 2);
-	//g.addEdge("C", "E", 1);
+	string inputHandler = "";
+	while (file >> inputHandler && !isdigit(inputHandler.at(0))) {
+		nodeNames.insert(nodeNames.end(), inputHandler);
+		file.ignore();
+	}
+	// Create the nodes for the graph
+	for (int i = 0; i < nodeNames.size(); i++) {
+		node a;
+		a.name = nodeNames.at(i);
+		nodeList.insert(nodeList.end(), a);
+	}
+	Graph g(nodeList.size());
+	// Insert the nodes into the graph
+	for (int i = 0; i < nodeList.size(); i++) {
+		g.addNode(nodeList.at(i));
+	}
 	g.print();
-	g.mst();
-	cout << endl;
-	g.print();
-	return 0;
+	inputHandler = "";
+	int row = 0;
+	while (getline(file, inputHandler, '\n')) {
+		
+	}
+	return 3;
 }
