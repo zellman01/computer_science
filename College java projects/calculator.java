567b241808262c0f0df925a92a754d794c001f57
Zach <18561222+LumiaNoivern@users.noreply.github.com>
2021-09-01 08:13:23 -0400

    Update calculator.java

--- College java projects/calculator.java
+++ College java projects/calculator.java
@@ -25,13 +25,14 @@ class MyCalculator extends JFrame implements ActionListener, DocumentListener {
 	JCheckBox inverse;
 	ButtonGroup bg;
 	
+	// Constructor
 	public MyCalculator() {
 		panelOne = new JPanel();
 		panelTwo = new JPanel();
-		squareRoot = new JRadioButton("Square Root");
-		sin = new JRadioButton("sin");
-		cos = new JRadioButton("cos");
-		ln = new JRadioButton("ln");
+		squareRoot = createButton("Square Root", "SQUAREROOT", true);
+		sin = createButton("Sin", "SIN");
+		cos = createButton("Cos", "COS");
+		ln = createButton("ln", "LN");
 		bg = new ButtonGroup();
 		input = new JTextField(20);
 		inputLabel = new JLabel("Input:");
@@ -40,16 +41,9 @@ class MyCalculator extends JFrame implements ActionListener, DocumentListener {
 		clear = new JButton("Clear");
 		inverse = new JCheckBox("Inverse");
 		
-		squareRoot.setActionCommand("SQUAREROOT");
-		sin.setActionCommand("SIN");
-		cos.setActionCommand("COS");
-		ln.setActionCommand("LN");
+		
 		clear.setActionCommand("CLEAR");
 		
-		squareRoot.addActionListener(this);
-		sin.addActionListener(this);
-		cos.addActionListener(this);
-		ln.addActionListener(this);
 		clear.addActionListener(this);
 		inverse.addActionListener(this);
 		input.getDocument().addDocumentListener(this);
@@ -80,6 +74,24 @@ class MyCalculator extends JFrame implements ActionListener, DocumentListener {
 		start();
 	}
 	
+	// Create a radio button from the label and action command
+	private JRadioButton createButton(String label, String command) {
+		JRadioButton a = new JRadioButton(label);
+		a.setActionCommand(command);
+		a.addActionListener(this);
+		return a;
+	}
+	
+	// Create a radio button from the label and action command
+	// Override from above to be able to specify selected status of the button
+	private JRadioButton createButton(String label, String command, boolean selected) {
+		JRadioButton a = new JRadioButton(label, selected);
+		a.setActionCommand(command);
+		a.addActionListener(this);
+		return a;
+	}
+	
+	// Start up the GUI aspect of the calculator
 	private void start() {
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		Dimension d = tk.getScreenSize();
@@ -92,12 +104,81 @@ class MyCalculator extends JFrame implements ActionListener, DocumentListener {
 		setVisible(true);
 	}
 	
+	// Does the calculation and displays it, based on the function.
+	// 1 = Square root, 2 = sin, 3 = cos, 4 = ln
+	// 5, 6, 7, 8 are reserved for only inside the function. They are the inverses of the above
+	private void calculate(int function, String number) {
+		if (!number.equals("")) {
+			double num = 0.0;
+			boolean cont = true, inverseOn = inverse.isSelected();
+			String outOfRange = "Number out of range.";
+			try {
+				num = Double.parseDouble(number);
+			} catch (NumberFormatException e) {
+				cont = false
+				output.setText("Invalid characters found.");
+			}
+			
+			if (cont) {
+				if (inverseOn) function += 4;
+				double ans;
+				switch (function) {
+					case 1:
+						if (num < 1) {
+							output.setText(outOfRange);
+						} else {
+							ans = Math.sqrt(num);
+							output.setText(String.valueOf(ans));
+						}
+						break;
+					case 2:
+						if (num < -1 || num > 1) {
+							output.setText(outOfRange);
+						} else {
+							ans = Math.sin(num);
+							output.setText(String.valueOf(ans));
+						}
+						break;
+					case 3:
+						//TODO: Look up
+						break;
+					case 4:
+						//TODO: Look up
+						break;
+				}
+			}
+		} else {
+			output.setText("");
+		}
+	}
+	
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("actionPerformed:");
-		System.out.print(e.getActionCommand() + "\n");
+		String inputString = input.getText().trim();
 		switch(e.getActionCommand()) {
+			case "CLEAR":
+				System.out.println("Clear found.");
+				input.setText("");
+				break;
+			case "SQUAREROOT":
+				System.out.println("Square Root found.");
+				calculate(1, inputString);
+				break;
+			case "SIN":
+				System.out.println("Sin found.");
+				calculate(2, inputString);
+				break;
+			case "COS":
+				System.out.println("Cos found.");
+				calculate(3, inputString);
+				break;
+			case "LN":
+				System.out.println("ln found.");
+				calculate(4, inputString);
+				break;
 			default:
-			System.out.println("No action found.");
+				System.out.println("No action found.");
+				break;
 		}
 	}
 	
@@ -107,9 +188,44 @@ class MyCalculator extends JFrame implements ActionListener, DocumentListener {
 	
 	public void insertUpdate(DocumentEvent e) {
 		System.out.println("insertUpdate");
+		String inputString = input.getText().trim();
+		switch (bg.getSelection().getActionCommand()) {
+			case "SQUAREROOT":
+				calculate(1, inputString);
+				break;
+			case "SIN":
+				calculate(2, inputString);
+				break;
+			case "COS":
+				calculate(3, inputString);
+				break;
+			case "LN":
+				calculate(4, inputString);
+				break;
+			default:
+				System.out.println("No action found.");
+				break;
+		}
 	}
 	
 	public void removeUpdate(DocumentEvent e) {
-		System.out.println("removeUpdate");
+		System.out.println("removeUpdate");String inputString = input.getText().trim();
+		switch (bg.getSelection().getActionCommand()) {
+			case "SQUAREROOT":
+				calculate(1, inputString);
+				break;
+			case "SIN":
+				calculate(2, inputString);
+				break;
+			case "COS":
+				calculate(3, inputString);
+				break;
+			case "LN":
+				calculate(4, inputString);
+				break;
+			default:
+				System.out.println("No action found.");
+				break;
+		}
 	}
 }
