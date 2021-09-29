267f307642ebdcd4c75df4918d4a3eca9379baab
Zach <18561222+zellman01@users.noreply.github.com>
2021-09-29 09:44:56 -0400

    Update ListManipulationProject.java

--- College java projects/ListManipulationProject.java
+++ College java projects/ListManipulationProject.java
@@ -4,6 +4,8 @@ import javax.swing.event.*;
 import java.lang.*;
 import java.awt.event.*;
 import java.io.*;
+import java.util.*;
+import java.text.*;
 
 public class ListManipulationProject {
 	public static void main(String[] args) {
@@ -12,15 +14,211 @@ public class ListManipulationProject {
 }
 
 class WorkOrder {
-	private double pay;
-	public WorkOrder() {
-		
+	private String name, dept, description;
+	private double billingRate;
+	private long initRequest, finishedRequest;
+
+	public WorkOrder() {}
+	
+	public WorkOrder(String name, String dept, String desc, double billRate, long initRequest, long finishedRequest) {
+		this.name = name;
+		this.dept = dept;
+		description = desc;
+		billingRate = billRate;
+		this.initRequest = initRequest;
+		this.finishedRequest = finishedRequest;
+	}
+	
+	public WorkOrder(DataInputStream dis) throws IOException {
+		name = dis.readUTF();
+		dept = dis.readUTF();
+		description = dis.readUTF();
+		billingRate = dis.readDouble();
+		initRequest = dis.readLong();
+		finishedRequest = dis.readLong();
+	}
+	
+	public static WorkOrder getRandom() {
+		WorkOrder randWO = new WorkOrder();
+		// name
+		// dept
+		randWO.description = "Random work order";
+		// billingRate
+		// random start date
+		// random end date
+		return randWO;
+	}
+	
+	private void consolePrint() {
+		System.out.println("-------Work Order debug-------");
+		System.out.println(name + "\n" + dept + "\n" + description);
+		System.out.println(billingRate);
+		// Format the bottom two to a date
+		System.out.println(initRequest);
+		System.out.println(finishedRequest);
+	}
+	
+	public void store(DataOutputStream dos) throws IOException {
+		dos.writeUTF(name);
+		dos.writeUTF(dept);
+		dos.writeUTF(description);
+		dos.writeDouble(billingRate);
+		dos.writeLong(initRequest);
+		dos.writeLong(finishedRequest);
 	}
 }
 
-class ListManip extends JFrame implements ActionListener, ListSelectionListener, MouseListener {
-	private JList<String> list;
-	private DefaultListModel<String> listViewer;
+interface Manager {
+	public void addWorkOrder(WorkOrder wo);
+	public void editWorkOrder();
+}
+
+class WorkOrderGUI extends JDialog implements ActionListener {
+	// TODO: Display a GUI for whenever someone selects add or edit
+	private Manager manager;
+	private final String[] departments = {"SALES", "HARDWARE", "ELECTRONICS"};
+	private JLabel name;
+	private JLabel department;
+	private JLabel date;
+	private JLabel date1;
+	private JLabel description;
+	private JLabel rate;
+	private JTextField nameInput;
+	private JTextField dateInput;
+	private JTextField dateInput1;
+	private JTextField descriptionInput;
+	private JTextField rateInput;
+	private JComboBox<String> departmentSelector;
+	private JButton save;
+	private JButton cancel;
+	
+	public WorkOrderGUI(Manager manager) {
+		JPanel panel = new JPanel();
+		JPanel panel1 = new JPanel();
+		this.manager = manager;
+		
+		name = new JLabel("Please input the person requesting the work.");
+		department = new JLabel("Please select the department that the work order is for.");
+		date = new JLabel("Please input the date that the request started.");
+		date1 = new JLabel("Please input the date that the request was fulfilled.");
+		description = new JLabel("Please input the description of the work order.");
+		rate = new JLabel("Please input the billing rate of this work order.");
+		
+		nameInput = new JTextField(20);
+		dateInput = new JTextField(20);
+		dateInput.setInputVerifier(new DateVerifier());
+		dateInput1 = new JTextField(20);
+		dateInput1.setInputVerifier(new DateVerifier());
+		descriptionInput = new JTextField(20);
+		rateInput = new JTextField(20);
+		rateInput.setInputVerifier(new RateVerifier());
+		
+		departmentSelector = new JComboBox<>(departments);
+		
+		save = makeButton("Save");
+		cancel = makeButton("Cancel");
+		
+		panel.add(name);
+		panel.add(nameInput);
+		panel.add(department);
+		panel.add(departmentSelector);
+		panel.add(date);
+		panel.add(dateInput);
+		panel.add(date1);
+		panel.add(dateInput1);
+		panel.add(description);
+		panel.add(descriptionInput);
+		panel.add(rate);
+		panel.add(rateInput);
+		
+		panel1.add(save);
+		panel1.add(cancel);
+		
+		add(panel1, BorderLayout.SOUTH);
+		
+		add(panel);
+		start();
+	}
+	
+	private void start() {
+		Toolkit tk;
+		Dimension d;
+		
+		setTitle("Add/Edit WorkOrder");
+		tk = Toolkit.getDefaultToolkit();
+		d = tk.getScreenSize();
+		setSize(d.width/4, (d.height/4)+83);
+		setLocation(d.width/4, d.height/4);
+		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		setVisible(true);
+		
+	}
+	
+	private JButton makeButton(String name) {
+		JButton b = new JButton(name);
+		b.setActionCommand(name.toUpperCase());
+		b.addActionListener(this);
+		return b;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("SAVE")) {
+			// Make sure that it is all correct
+			// Create a WorkOrder object
+			// Use manager.addWorkOrder(WorkOrder)
+		}
+		// Close window without doing anything when cancel is clicked
+	}
+}
+
+class RateVerifier extends InputVerifier {
+	public boolean verify(JComponent input) {
+		double rate;
+		boolean valid;
+		String str;
+		
+		str = ((JTextField)input).getText().trim();
+		
+		try {
+			rate = Double.parseDouble(str);
+			valid = true;
+		} catch(NumberFormatException e) {
+			JOptionPane.showMessageDialog(input.getParent(), "The billing rate field must be a double.", "Error", JOptionPane.ERROR_MESSAGE);
+			valid = false;
+		}
+		
+		return valid;
+	}
+}
+
+class DateVerifier extends InputVerifier {
+	public boolean verify(JComponent input) {
+		String str;
+		Date d;
+		SimpleDateFormat df;
+		ParsePosition pos;
+		boolean valid;
+		
+		str = ((JTextField)input).getText().trim();
+		
+		df = new SimpleDateFormat("MM/dd/yy");
+		df.setLenient(false);
+		
+		pos = new ParsePosition(0);
+		d = df.parse(str, pos);
+		valid = pos.getIndex() == str.length() && d != null;
+		if (!valid) {
+			JOptionPane.showMessageDialog(input.getParent(), "The date needs to be in the format MM/dd/yy", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		return valid;
+	}
+}
+
+class ListManip extends JFrame implements ActionListener, ListSelectionListener, MouseListener, Manager {
+	private JList<WorkOrder> list;
+	private DefaultListModel<WorkOrder> listViewer;
 	private JFileChooser files;
 	private File chosenFile;
 	private JButton load;
@@ -37,8 +235,8 @@ class ListManip extends JFrame implements ActionListener, ListSelectionListener,
 		
 		files = new JFileChooser(".");
 		
-		listViewer = new DefaultListModel<String>();
-		list = new JList<String>(listViewer);
+		listViewer = new DefaultListModel<WorkOrder>();
+		list = new JList<WorkOrder>(listViewer);
 		list.addListSelectionListener(this);
 		
 		
@@ -101,7 +299,7 @@ class ListManip extends JFrame implements ActionListener, ListSelectionListener,
 		JMenuItem m;
 		
 		m = new JMenuItem(label, mnemonic);
-		m.setAccelerator(KeyStroke.getKeyStroke(keyCode, KeyEvent.ALT_MASK));
+		m.setAccelerator(KeyStroke.getKeyStroke(keyCode, KeyEvent.ALT_DOWN_MASK));
 		m.setToolTipText(toolTip);
 		m.setActionCommand(actionCommand);
 		m.addActionListener(listener);
@@ -138,25 +336,25 @@ class ListManip extends JFrame implements ActionListener, ListSelectionListener,
 	
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("ADD")) {
-			String str = JOptionPane.showInputDialog(this, "What would you like to add to the JList?").trim();
-			if (!str.equals(""))
-				listViewer.addElement(str);
+			new WorkOrderGUI(this);
 		}
 		
-		if (e.getActionCommand().equals("LOAD")) {
+		if (e.getActionCommand().equals("LOAD")) { // Use DataInputStream, and load from a file, using the WorkOrder constructor
 			// Open a file
 			
 			if (files.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
 				chosenFile = files.getSelectedFile();
 				try {
-					BufferedReader br = new BufferedReader(new FileReader(chosenFile));
-					do {
-						String add = br.readLine();
-						if (add == null) break;
-						listViewer.addElement(add);
-					} while (true);
+					int objectAmount;
+					//BufferedReader br = new BufferedReader(new FileReader(chosenFile));
+					DataInputStream dis = new DataInputStream(new FileInputStream(chosenFile));
+					// Read in the number of objects there were
+					objectAmount = dis.readInt();
+					for (int i = 0; i < objectAmount; i++) {
+						listViewer.addElement(new WorkOrder(dis)); // Should add in work orders from a file
+					}
 				} catch (IOException e1) {
-					
+					e1.printStackTrace();
 				}
 			}
 		}
@@ -203,14 +401,16 @@ class ListManip extends JFrame implements ActionListener, ListSelectionListener,
 	
 	private void saveObject() {
 		try {
-			PrintWriter pw = new PrintWriter(chosenFile);
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream(chosenFile));
 			Object allElements[] = listViewer.toArray();
 			for (int i = 0; i < allElements.length; i++) {
-				pw.println((String)(allElements[i]));
+				WorkOrder temp = (WorkOrder)(allElements[i]);
+				temp.store(dos);
 			}
-			pw.flush();
-			pw.close();
-		} catch (FileNotFoundException e1) {		
+			dos.flush();
+			dos.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 	}
 	
@@ -238,4 +438,9 @@ class ListManip extends JFrame implements ActionListener, ListSelectionListener,
 		
 	}
 	
+	public void addWorkOrder(WorkOrder wo) {
+		listViewer.addElement(wo);
+	}
+	
+	public void editWorkOrder() {}
 }
