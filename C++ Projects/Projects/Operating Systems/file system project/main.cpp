49c3b06a8ef67f6e2f3431c99b880102e7938fce
Zach <18561222+zellman01@users.noreply.github.com>
2021-11-10 08:33:28 -0500

    Today's updates

--- C++ Projects/Projects/Operating Systems/file system project/main.cpp
+++ C++ Projects/Projects/Operating Systems/file system project/main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 struct FileBlock {
 	char name[11];
-	int location;
+	int location; // 0 - 49
 	int size;
+	bool inUse;
 };
 
 struct Data {
@@ -21,9 +22,6 @@ void createFileSystem();
 void readFileSystem();
 void menu(int);
 void manageFileSystem(char*, int);
-void createFile();
-void readFile();
-void deleteFile();
 
 int main() {
 	int choice = 0;
@@ -55,28 +53,105 @@ void readFileSystem() {
 
 void menu(int mode) {
 	if (mode == 1) {
-		cout << "Options:" << endl << "1) Create a file" << endl;
+		cout << "Options:" << endl << "1) Create a file" << endl << "-1) Stop" << endl;
 		return;
 	}
-	cout << "Options:" << endl << "1) Create a file" << endl << "2) Read a file" << endl << "3) Delete a file" << endl;
+	cout << "Options:" << endl << "1) Create a file" << endl << "2) Read a file" << endl << "3) Delete a file" << endl << "-1) Stop" << endl;
 }
 
 void manageFileSystem(char * fileName, int mode) {
-	FileBlock fb[20]; // 20 files in the system, max 
+	FileBlock fb[20]; // 20 files in the system, max
+	for (int i = 0; i < 20; i++) fb[i].inUse = false;
 	// 1 = newly created file system
 	// 2 = already created file system
 	FreeIndex fi[50]; // 50 free indexes (100 characters)
 	Data d[50]; // 50 blocks of data
 	for (int i = 0; i < 50; i++) {
+		fi[i].isFree = true;
 		fi[i].dataBlock = &d[i];
 	}
 	ofstream fs(fileName);
 	int choice = 0;
 	
-	if (mode == 1) {
-		do {
-			menu(1);
-			cin >> choice;
-		} while (choice != 1);
-	}
+	do {
+		if (mode == 1) {
+			do {
+				menu(1);
+				cin >> choice;
+			} while (choice != 1 || choice != -1);
+		} else if (mode == 2) {
+			do {
+				menu(2);
+				cin >> choice;
+			} while (choice < -1 || choice > 3 || choice == 0);
+		}
+		char fName[11];
+		int fileBlockPosition, freeIndexPosition;
+		// Do the menu choice
+		switch(choice) {
+		case 1:
+			// Create a file in the filesystem
+			cout << "Input the name of the file you want to use: ";
+			cin >> fName;
+			fileBlockPosition = -1;
+			bool nameInUse = false;
+			for (int i = 0; i < 20; i++) {
+				if (!fb[i].inUse && fileBlockPosition == -1) fileBlockPosition = i;
+				if (fb[i].inUse && strcmp(fb[i].name, fName) == 0) {
+					nameInUse = true;
+				}
+			}
+			
+			if (nameInUse) cout << "The name you choose already exists in the filesystem." << endl;
+			else {
+				if (fileBlockPosition == -1) cout << "There is no more room for a file." << endl;
+				else {
+					freeIndexPosition = -1;
+					for (int i = 0; i < 50 && freeIndexPosition == -1; i++) {
+						if (fi[i].isFree) freeIndexPosition = i;
+					}
+					if (freeIndexPosition == -1) cout << "There is no more room for a file." << endl;
+					else {
+						fb[fileBlockPosition].name = fName;
+						fb[fileBlockPosition].location = freeIndexPosition;
+						fb[fileBlockPosition].size = 0; // Get the text they want to use, and then put it in here (or use the amount of free indexes they used for it)
+						fb[fileBlockPosition].inUse = true;
+						fi[freeIndexPosition].isFree = false;
+						fi[freeIndexPosition].dataBlock->character = '\0'; // null terminator for now - get a way to put the data in the block, and add more if neccessary.
+					}
+				}
+			}
+			break;
+		case 2:
+			cout << "Input the file name that you want to read: ";
+			cin >> fName;
+			fileBlockPosition = -1;
+			for (int i = 0; i < 20 && fileBlockPosition == -1; i++) {
+				if (fb[i].inUse && strcmp(fb[i].name, fName) == 0) {
+					fileBlockPosition = i;
+				}
+			}
+			if (fileBlockPosition == -1) {
+				cout << "The file could not be found." << endl;
+			} else {
+				// Get to the address of the Data block from the FreeIndex array, and then read from there
+			}
+			break;
+		case 3:
+			// Remove a file in the filesystem
+			cout << "Input the name of the file you want to delete: ";
+			cin >> fName;
+			fileBlockPosition = -1;
+			for (int i = 0; i < 20 && fileBlockPosition == -1; i++) {
+				if (strcmp(fb[i].name, fName) == 0) {
+					fileBlockPosition = i;
+				}
+			}
+			if (fileBlockPosition == -1) cout << "There is no such file with that name." << endl;
+			else {
+				fb[fileBlockPosition].inUse = false; // Logically delete the file
+			}
+			break;
+		}
+	} while (choice != -1);
 }
